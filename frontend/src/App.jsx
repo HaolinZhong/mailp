cc23cb65162829db125ff2626b348695f65f5885
HaolinZhong <hz2771@columbia.edu>
2022-06-26 19:52:40 -0400

    add header template for website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+
+const App = () => {
+    return (
+        <Header />
+    )
+}
+
+export default App
